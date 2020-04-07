8e1b8d14665af21f50d722b33bb26a2f368ff598
jhiggs <higgsj82@gmail.com>
2020-04-07 13:23:49 -0400

    Remove logger from store config

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+// import logger from "redux-logger";s
 import thunk from "redux-thunk";
 import rootReducer from "../reducers/root_reducer";
 
 const bumpifyStore = (preloadedState = {}) =>
-    createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger));
+    createStore(rootReducer, preloadedState, applyMiddleware(thunk));
 
 export default bumpifyStore;
